2a0e38a17f866ec0d3e10048513bde448b4d8442
Piotr Zdzieblowski <piotr@zdzieblowski.com>
2022-09-07 22:48:28 +0200

    Update overclockTemplateOverSSH.sh

--- overclockTemplateOverSSH.sh
+++ overclockTemplateOverSSH.sh
@@ -1,13 +1,42 @@
 #!/bin/sh
 
+# ENABLE PERSISTENCE
 sudo nvidia-smi -pm 1
 
-sudo nvidia-smi -pl 125 -i 0
-sudo nvidia-smi -pl 125 -i 1
-sudo nvidia-smi -pl 125 -i 2
-sudo nvidia-smi -pl 125 -i 3
+# SET POWER LIMIT
+sudo nvidia-smi -pl 123 -i 0
+#sudo nvidia-smi -pl 123 -i 1
+#sudo nvidia-smi -pl 123 -i 2
+#sudo nvidia-smi -pl 123 -i 3
 
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffsetAllPerformanceLevels=2400
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffsetAllPerformanceLevels=2400
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffsetAllPerformanceLevels=2400
-DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffsetAllPerformanceLevels=2400
+### ALL PERFORMANCE LEVELS
+
+# SET CORE CLOCK OFFSET 
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:0]/GPUGraphicsClockOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:1]/GPUGraphicsClockOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:2]/GPUGraphicsClockOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:3]/GPUGraphicsClockOffsetAllPerformanceLevels=0
+
+# SET MEMORY TRANSFER RATE OFFSET
+DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffsetAllPerformanceLevels=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffsetAllPerformanceLevels=0
+
+###
+
+### SPECIFY PERFORMANCE LEVELS
+
+# SET CORE CLOCK OFFSET 
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:0]/GPUGraphicsClockOffset[0]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:1]/GPUGraphicsClockOffset[0]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:2]/GPUGraphicsClockOffset[0]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:3]/GPUGraphicsClockOffset[0]=0
+
+# SET MEMORY TRANSFER RATE OFFSET
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:0]/GPUMemoryTransferRateOffset[]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:1]/GPUMemoryTransferRateOffset[]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:2]/GPUMemoryTransferRateOffset[]=0
+#DISPLAY=:0 XAUTHORITY=/var/run/lightdm/root/:0 nvidia-settings -a [gpu:3]/GPUMemoryTransferRateOffset[]=0
+
+###
